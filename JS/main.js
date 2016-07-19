19cabbc87783e2b990956469584da7374a49b789
Hendy Budhigraha <htan1201@outlook.com>
2016-07-19 18:18:13 +0800

    create a proper function for navigation toggle

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,14 @@
-function myFunction() {
+//Function for the toggle function in the navbar
+function naviToggle() {
     document.getElementsByClassName("topnavi")[0].classList.toggle("responsive");
 }
 
+//Function for the slides
 $(document).ready(function(){
   $('.slides').slick({
     accessibility: true,
-    dots: true
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 4000
   });
 });
